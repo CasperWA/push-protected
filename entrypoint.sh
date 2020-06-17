38ce50ced36f26bf3a9ca5540bffc512690f0ab7
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-17 07:21:37 +0200

    Try new way of passing auth info

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ git config --system user.name "${GITHUB_WORKFLOW}"
 mkdir target_repo
 cd target_repo
 git init
-git pull https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git
+git pull https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git
 
 if [ "${GITHUB_SHA}" != "$(git rev-parse HEAD)" ]; then
     git checkout -f "$(python -c "print('/'.join('${GITHUB_REF}'.split('/')[2:]))")"
@@ -20,7 +20,7 @@ fi
 
 # Retrieve shell script to run changes
 if [ -n "${INPUT_CHANGES}" ]; then
-    wget --tries=5 https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/${GITHUB_SHA}/${INPUT_CHANGES}
+    wget --tries=5 https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@raw.githubusercontent.com/${GITHUB_REPOSITORY}/${GITHUB_SHA}/${INPUT_CHANGES}
 
     FILENAME=$(python -c "import os; print(os.path.basename('${INPUT_CHANGES}'))")
     /bin/sh ${FILENAME}
@@ -29,7 +29,7 @@ fi
 # Create new temporary repository
 TEMPORARY_BRANCH="push-action/temporary/${GITHUB_RUN_ID}"
 git checkout -f -b ${TEMPORARY_BRANCH}
-git push -f https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git ${TEMPORARY_BRANCH}
+git push https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git ${TEMPORARY_BRANCH}
 
 # Wait for status checks to complete
 python ../app/run.sh --token "${INPUT_GITHUB_TOKEN}" --repo "${INPUT_REPOSITORY}" --run-id "${GITHUB_RUN_ID}" --ref "${INPUT_BRANCH}" wait_for_checks
@@ -37,7 +37,7 @@ python ../app/run.sh --token "${INPUT_GITHUB_TOKEN}" --repo "${INPUT_REPOSITORY}
 # Merge into target branch
 git checkout -f ${INPUT_BRANCH}
 git merge --ff-only ${TEMPORARY_BRANCH}
-git push https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git ${INPUT_BRANCH}
+git push https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git ${INPUT_BRANCH}
 
 # Remove temporary repository
 python ../app/run.sh --token "${INPUT_GITHUB_TOKEN}" --repo "${INPUT_REPOSITORY}" --run-id "${GITHUB_RUN_ID}" --ref "${INPUT_BRANCH}" remove_temp_branch
