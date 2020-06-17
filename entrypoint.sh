e17230b8f516c5d6a82232d1d14f4ae0c9d4042d
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-17 07:15:36 +0200

    Don't use clone. add 'x-oauth-basic' as pw

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,10 @@ git config --system user.email "actions@github.com"
 git config --system user.name "${GITHUB_WORKFLOW}"
 
 # Retrieve target repository
-git clone https://${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git target_repo/
+mkdir target_repo
 cd target_repo
+git init
+git pull https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git
 
 if [ "${GITHUB_SHA}" != "$(git rev-parse HEAD)" ]; then
     git checkout -f "$(python -c "print('/'.join('${GITHUB_REF}'.split('/')[2:]))")"
@@ -27,15 +29,15 @@ fi
 # Create new temporary repository
 TEMPORARY_BRANCH="push-action/temporary/${GITHUB_RUN_ID}"
 git checkout -f -b ${TEMPORARY_BRANCH}
-git push -f origin ${TEMPORARY_BRANCH}
+git push -f https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git ${TEMPORARY_BRANCH}
 
 # Wait for status checks to complete
 python ../app/run.sh --token "${INPUT_GITHUB_TOKEN}" --repo "${INPUT_REPOSITORY}" --run-id "${GITHUB_RUN_ID}" --ref "${INPUT_BRANCH}" wait_for_checks
 
 # Merge into target branch
-git checkout -f -t origin/${INPUT_BRANCH} ${INPUT_BRANCH}
-git merge --ff-only origin/${TEMPORARY_BRANCH}
-git push
+git checkout -f ${INPUT_BRANCH}
+git merge --ff-only ${TEMPORARY_BRANCH}
+git push https://${INPUT_GITHUB_TOKEN}:x-oauth-basic@github.com/${INPUT_REPOSITORY}.git ${INPUT_BRANCH}
 
 # Remove temporary repository
 python ../app/run.sh --token "${INPUT_GITHUB_TOKEN}" --repo "${INPUT_REPOSITORY}" --run-id "${GITHUB_RUN_ID}" --ref "${INPUT_BRANCH}" remove_temp_branch
