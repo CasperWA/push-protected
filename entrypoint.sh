2017320025d57e50fea80f4b70f979b082d69695
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-18 19:40:01 +0200

    Remove /dev/null redirect - not working

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,19 @@
 
 # Retrieve target repository
 echo "Getting latest commit of ${INPUT_REPOSITORY}@${INPUT_BRANCH} ..."
-git clone https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git target_repo/ > /dev/null
+git clone https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git target_repo/
 cd target_repo
 
 # Setup git user
 git config user.email "actions@github.com"
 git config user.name "${GITHUB_ACTOR}"
 
-git checkout -f ${INPUT_BRANCH} > /dev/null
+git checkout -f ${INPUT_BRANCH}
 echo "Getting latest commit of ${INPUT_REPOSITORY}@${INPUT_BRANCH} ... DONE!"
 
 # Retrieve shell script to run changes
 if [ -n "${INPUT_CHANGES}" ]; then
-    wget --tries=5 https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@raw.githubusercontent.com/${GITHUB_REPOSITORY}/${GITHUB_SHA}/${INPUT_CHANGES} > /dev/null
+    wget --tries=5 https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@raw.githubusercontent.com/${GITHUB_REPOSITORY}/${GITHUB_SHA}/${INPUT_CHANGES}
 
     FILENAME=$(python -c "import os; print(os.path.basename('${INPUT_CHANGES}'))")
 
@@ -26,8 +26,8 @@ fi
 # Create new temporary repository
 TEMPORARY_BRANCH="push-action/${GITHUB_RUN_ID}/$(uuidgen)"
 echo "Creating temporary repository '${TEMPORARY_BRANCH}' ..."
-git checkout -f -b ${TEMPORARY_BRANCH} > /dev/null
-git push -f origin ${TEMPORARY_BRANCH} > /dev/null
+git checkout -f -b ${TEMPORARY_BRANCH}
+git push -f origin ${TEMPORARY_BRANCH}
 echo "Creating temporary repository '${TEMPORARY_BRANCH}' ... DONE!"
 
 {
@@ -36,9 +36,9 @@ echo "Creating temporary repository '${TEMPORARY_BRANCH}' ... DONE!"
 
     # Merge into target branch
     echo "Merging (fast-forward) '${TEMPORARY_BRANCH}' -> '${INPUT_BRANCH}' ..." &&
-    git checkout ${INPUT_BRANCH} > /dev/null &&
-    git merge --ff-only origin/${TEMPORARY_BRANCH} > /dev/null &&
-    git push > /dev/null &&
+    git checkout ${INPUT_BRANCH} &&
+    git merge --ff-only origin/${TEMPORARY_BRANCH} &&
+    git push &&
     echo "Merging (fast-forward) '${TEMPORARY_BRANCH}' -> '${INPUT_BRANCH}' ... DONE!" &&
 
     # Remove temporary repository
