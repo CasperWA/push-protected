8013fdb4f2c1d9d34ac882a1f89b4eb69708e290
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-19 01:55:33 +0200

    Add test for creating file directly in workflow

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -4,5 +4,5 @@ set -e
 git checkout -b ci_tests
 echo "This is a test. ${RANDOM} ${RANDOM}" >> "ci_test_file.txt"
 mv -f extra_data_more.md .github/
-git add ci_test_file.txt extra_data.log .github/extra_data_more.md
+git add ci_test_file.txt extra_data.log .github/extra_data_more.md Workflow_file.txt
 git commit -m "CI tests"
