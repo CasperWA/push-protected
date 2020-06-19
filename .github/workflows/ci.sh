4b7263975c950a594679ab21a820791f5da92eda
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-19 02:29:07 +0200

    Try installing Ruby in script

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -4,5 +4,15 @@ set -e
 git checkout -b ci_tests
 echo "This is a test. ${RANDOM} ${RANDOM}" >> "ci_test_file.txt"
 mv -f extra_data_more.md .github/
-git add ci_test_file.txt extra_data.log .github/extra_data_more.md Workflow_file.txt
+
+echo "Test installing Ruby"
+apt-get update
+apt-get install ruby-full build-essential zlib1g-dev
+export GEM_HOME="$HOME/gems"
+export PATH="$GEM_HOME/bin:$PATH"
+
+echo "Test installing gem"
+gem install jekyll
+
+git add ci_test_file.txt extra_data.log .github/extra_data_more.md
 git commit -m "CI tests"
