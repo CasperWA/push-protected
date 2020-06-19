4939becc7bb8c0e4e61ed9e7f941e3a55b6fc822
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-19 02:42:08 +0200

    Remove ruby again

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -5,14 +5,5 @@ git checkout -b ci_tests
 echo "This is a test. ${RANDOM} ${RANDOM}" >> "ci_test_file.txt"
 mv -f extra_data_more.md .github/
 
-echo "Test installing Ruby"
-apt-get update
-apt-get -y install ruby-full build-essential zlib1g-dev
-export GEM_HOME="$HOME/gems"
-export PATH="$GEM_HOME/bin:$PATH"
-
-echo "Test installing gem"
-gem install jekyll
-
 git add ci_test_file.txt extra_data.log .github/extra_data_more.md
 git commit -m "CI tests"
