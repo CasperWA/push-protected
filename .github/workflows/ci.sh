b87e7171bb4297c980ba88aab374b097884b417f
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-08-04 10:18:04 +0200

    Add test for force-pushing tags

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
+TAG_NAME=$1
+if [ -z "${TAG_NAME}" ]; then
+    TAG_NAME=ci_test
+fi
+
 git config user.email "casper+github@welzel.nu"
 git config user.name "Casper Welzel Andersen"
 
@@ -10,3 +15,5 @@ mv -f ci_test_file.txt ../../
 
 git add ../../ci_test_file.txt ../extra_data.log extra_data_more.md
 git commit -m "CI tests"
+
+git tag ci_test -a -m "This is a test tag"
