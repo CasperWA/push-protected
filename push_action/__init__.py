00b7191ca6fd1c0e4eafcc432326201c014fc314
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2024-01-31 09:32:45 +0100

    Update `pre-commit` hooks (#223)

--- push_action/__init__.py
+++ push_action/__init__.py
@@ -3,6 +3,7 @@
 This Python module is a helper module to interact with the GitHub API.
 It is meant to only be used in the `push-protected` GitHub action.
 """
+
 import logging
 import os
 import sys
--- push_action/validate.py
+++ push_action/validate.py
@@ -1,4 +1,5 @@
 """Validate inputs."""
+
 from __future__ import annotations
 
 from urllib.parse import urlsplit
